a7b686c44a8e5c9e3dcdbf6970ce1c68bfa7b25a
Diar322 <superboopy@gmail.com>
2021-08-03 20:22:49 +0200

    Start Commit

---
+++ Diars Script Pack/scripts/server/myServerScript.js
@@ -0,0 +1,18 @@
+let sampleServerSystem = server.registerSystem(0, 0);
+
+// void start()
+sampleSystem.initialize = function() {
+	//register event data, register components, register queries, listen for events
+};z
+
+
+//update 20 times a sec
+sampleSystem.update = function() {
+	//Update all the things
+};
+
+
+//on exit
+sampleSystem.shutdown = function() {
+	//Cleanup script only things
+};
