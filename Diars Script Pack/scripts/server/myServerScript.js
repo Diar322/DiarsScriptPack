8b3db6d533c3b5b5db884bdbac9722d542c043af
Diar322 <superboopy@gmail.com>
2021-11-07 09:39:10 +0100

    Spawns creeper, kind of

--- Diars Script Pack/scripts/server/myServerScript.js
+++ Diars Script Pack/scripts/server/myServerScript.js
@@ -1,32 +1,52 @@
-let sampleServerSystem = server.registerSystem(0, 0);
+// this signs-up this script to run on the server-side of the Minecraft Bedrock Edition engine
+const systemServer = server.registerSystem(0, 0)
 
-// void start()
-sampleServerSystem.initialize = function() {
-	//register event data, register components, register queries, listen for events
-	log("bruh");
-	this.listenForEvent("minecraft:entity_acquired_item", (eventData) => this.onItemEvent(eventData));
-};
+// the server runs this as soon as the scripts are all fully loaded
+systemServer.initialize = function () {
+	this.listenForEvent("minecraft:entity_acquired_item", (eventData) => this.onItem(eventData));
 
+	// turn on logging of information, warnings, and errors
 
-//update 20 times a sec
-sampleServerSystem.update = function() {
-	//Update all the things
-};
+	var scriptLoggerConfig = this.createEventData("minecraft:script_logger_config");
+    scriptLoggerConfig.data.log_errors = true;
+    scriptLoggerConfig.data.log_information = true;
+    scriptLoggerConfig.data.log_warnings = true;
+    this.broadcastEvent("minecraft:script_logger_config", scriptLoggerConfig);
 
+	// register event data, register components, register queries, listen for events, . . .
 
-//on exit
-sampleServerSystem.shutdown = function() {
-	//Cleanup script only things
-};
+}
 
-sampleServerSystem.onItemEvent = function(eventData) {
-	if (eventData.item_stack.__identifier__ == "minecraft:egg")
-	{
-		log("egg")
-	}
+systemServer.onItem = function(eventData) {
+	let entity = this.createEntity('entity', "minecraft:creeper");
+
+	let component = this.getComponent(entity, "minecraft:health");
+
+	//let expl = this.createComponent(entity, "minecraft:scale");
+	let damageSensorComponent = systemServer.getComponent(entity, "minecraft:damage_sensor");
+	damageSensorComponent.data[0].on_damage = { event:"minecraft:start_exploding", filters:[{test:"has_component", operator:"==", value:"minecraft:breathable"}] };
+	systemServer.applyComponentChanges(entity, damageSensorComponent);
+	//var comp1 = this.createComponent(entity, "minecraft:explode");
+
+	//this.applyComponentChanges(entity, comp1);
+
 };
   
-sampleServerSystem.log = function (...items) {
+
+// the server runs this update function 20 times per second
+systemServer.update = function () {
+	// print hello world to the world's chat once per second
+
+	// update other stuff . . .
+}
+
+// the server only runs this when the world is shutting down
+systemServer.shutdown = function () {
+	// clean up stuff . . .
+}
+
+// This is just a helper function that simplifies logging data to the console.
+systemServer.log = function (...items) {
 	// Convert every parameter into a legible string and collect them into an array.
 	const toString = (item) => {
 		switch (Object.prototype.toString.call(item)) {
